3f113f9cee4bb02fd974dfcf86070ac19840caa6
HiagoBriano <hiago.artist@hotmail.com>
2022-02-12 18:04:53 -0300

    aviso que a pagina esta em construcao feito

--- src/Pages/Choice/index.js
+++ src/Pages/Choice/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Construction from '../../images/Construction.png';
 import Drink from '../../images/Drink.gif';
 import Favorite from '../../images/Favorite.gif';
 import Meal from '../../images/Meal.gif';
@@ -6,44 +7,68 @@ import Profile from '../../images/Profile.gif';
 import './Choice.css';
 
 function Choice({ history }) {
-  // const limp = () => {
-  //   console.log('ola');
-  //   localStorage.clear();
-  //   history.push('/');
-  // };
+  const [popup, setPopup] = useState(false);
+
+  const limp = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  const clickPopup = () => {
+    setPopup(!popup);
+  };
 
   return (
-    <>
-      <div className="choice-cards">
-        <div class="card">
-          <div class="card-body">
-            <img className="img-drinks" src={Meal} alt="comida" />
-            <span class="btn">comida</span>
-          </div>
+    <div className="choice-cards">
+      {popup ? (
+        <div className="choice-popup">
+          <img src={Construction} alt="comida" />
+          <button
+            className="choice-popup-btn"
+            type="button"
+            data-testid="login-submit-btn"
+            onClick={() => clickPopup()}
+          >
+            Voltar ao menu
+          </button>
+          <button
+            className="choice-popup-btn"
+            type="button"
+            data-testid="login-submit-btn"
+            onClick={() => limp()}
+          >
+            Ir para a tela inicial
+          </button>
         </div>
-
-        <div class="card">
-          <div class="card-body">
-            <img className="img-drinks" src={Drink} alt="comida" />
-            <span class="btn">Bebida</span>
+      ) : (
+        <>
+          <div class="choice-cards-single" onClick={() => clickPopup()}>
+            <div class="choice-cards-single-childen">
+              <img src={Meal} alt="comida" />
+              <span>comida</span>
+            </div>
           </div>
-        </div>
-
-        <div class="card">
-          <div class="card-body">
-            <img className="img-drinks" src={Favorite} alt="comida" />
-            <span class="btn">Receitas favoritas</span>
+          <div class="choice-cards-single" onClick={() => clickPopup()}>
+            <div class="choice-cards-single-childen">
+              <img src={Drink} alt="comida" />
+              <span>Bebida</span>
+            </div>
           </div>
-        </div>
-
-        <div class="card">
-          <div class="card-body">
-            <img className="img-drinks" src={Profile} alt="comida" />
-            <span class="btn">Perfil</span>
+          <div class="choice-cards-single" onClick={() => clickPopup()}>
+            <div class="choice-cards-single-childen">
+              <img src={Favorite} alt="comida" />
+              <span>Receitas favoritas</span>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
+          <div class="choice-cards-single" onClick={() => clickPopup()}>
+            <div class="choice-cards-single-childen">
+              <img src={Profile} alt="comida" />
+              <span>Perfil</span>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
