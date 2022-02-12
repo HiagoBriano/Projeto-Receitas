2104a694700c836e3b42dd0bd7bf97d9a95f41e5
HiagoBriano <hiago.artist@hotmail.com>
2022-02-12 02:58:07 -0300

    adicionei o botao de voltar na segunda pagina

--- src/Pages/Choice/index.js
+++ src/Pages/Choice/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-function Choice() {
+function Choice({ history }) {
+  const limp = () => {
+    console.log('ola');
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <h1>Escolha, comida ou bebida</h1>
       <p>e programe uma deliciosa receita</p>
+      <button onClick={() => limp()}>
+        Clique aqui para voltar a pagina inicial
+      </button>
     </div>
   );
 }
