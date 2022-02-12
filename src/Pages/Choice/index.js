407c1264bfb5f50ba3562dbb5c53757110980a17
HiagoBriano <hiago.artist@hotmail.com>
2022-02-12 17:09:42 -0300

    pagina de choice pronta

--- src/Pages/Choice/index.js
+++ src/Pages/Choice/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import Drink from '../../images/Drink.gif';
+import Favorite from '../../images/Favorite.gif';
+import Meal from '../../images/Meal.gif';
+import Profile from '../../images/Profile.gif';
+import './Choice.css';
 
 function Choice({ history }) {
-  const limp = () => {
-    console.log('ola');
-    localStorage.clear();
-    history.push('/');
-  };
+  // const limp = () => {
+  //   console.log('ola');
+  //   localStorage.clear();
+  //   history.push('/');
+  // };
 
   return (
-    <div>
-      <h1>Escolha, comida ou bebida</h1>
-      <p>e programe uma deliciosa receita</p>
-      <button onClick={() => limp()}>
-        Clique aqui para voltar a pagina inicial
-      </button>
-    </div>
+    <>
+      <div className="choice-cards">
+        <div class="card">
+          <div class="card-body">
+            <img className="img-drinks" src={Meal} alt="comida" />
+            <span class="btn">comida</span>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <img className="img-drinks" src={Drink} alt="comida" />
+            <span class="btn">Bebida</span>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <img className="img-drinks" src={Favorite} alt="comida" />
+            <span class="btn">Receitas favoritas</span>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-body">
+            <img className="img-drinks" src={Profile} alt="comida" />
+            <span class="btn">Perfil</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
